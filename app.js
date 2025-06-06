31ca10279882ce89992e8258715b68117047e205
Shakif Ahmed <213491867+shakif-2025@users.noreply.github.com>
2025-06-06 23:53:10 +0600

    Add files via upload
    how to add internal js

---
+++ app.js
@@ -0,0 +1,18 @@
+// console.log("Hello javaScript");
+
+// document.getElementById("title").innerHTML = "Hello world";
+
+// let sum = 70 + 50;
+
+// console.log('sum');
+
+// if(sum >100){
+//     console.log('Expensive')
+
+// } else('cheep')
+
+//alert("Hello");
+
+console.log(100);
+
+document.write("Internal JS");
