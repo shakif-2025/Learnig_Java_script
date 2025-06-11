2fe6e421724a20957d4a17209387d02a403f9119
Shakif Ahmed <213491867+shakif-2025@users.noreply.github.com>
2025-06-11 19:50:34 +0600

    Add files via upload
    commit done

---
+++ 007_personal_budget_tracker_app_phase_1,2/app.js
@@ -0,0 +1,19 @@
+// Phase 01 starts
+console.log("personal_budget_tracker_app_phase_1");
+console.log("My name is shakif Ahmed");
+console.log(4567); //Income
+console.log(2357); //expence
+// Phase 01 ends
+
+// Phase 02 starts
+
+const userName = "Shakif Ahmed";
+const income = 5290; // income
+const expence = 3510; //expence
+
+console.log("personal_budget_tracker_app_phase_2");
+console.log(userName);
+console.log(income);
+console.log(expence);
+
+// Phase 02 ends
---
+++ 008_Number_methode/number.js
@@ -0,0 +1,18 @@
+console.log(Number(false));
+console.log(typeof Number("25"));
+
+var num = 100.2547;
+console.log("What is the dataType of this variable:");
+
+num = toString(num);
+
+num = parseInt(num);
+
+num = parseFloat(num);
+console.log(typeof num);
+
+var number = 5.2546547;
+
+console.log(number.toFixed(3));
+
+console.log(number.toPrecision(5));
